966b5c9da9054962f82de54304d25a1bcf2d75da
Irina Babenko <babenko8irina@gmail.com>
2024-08-13 18:19:04 +0300

    correction FrendListItem.jsx and css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,3 +1,22 @@
+// import PropTypes from 'prop-types';
+// import styles from './FriendListItem.module.css';
+
+// export default function FriendListItem({ avatar, name, isOnline }) {
+//   return (
+//     <div>
+//       <img src={avatar} alt="Avatar" width="48" className={styles.friendsImg} />
+//       <p className={styles.friendName}>{name}</p>
+//       <p
+//         className={
+//           (styles.friendStatus, isOnline ? styles.isOnline : styles.isOffline)
+//         }
+//       >
+//         {isOnline ? 'Online' : 'Offline'}
+//       </p>
+//     </div>
+//   );
+// }
+
 import PropTypes from 'prop-types';
 import styles from './FriendListItem.module.css';
 
@@ -7,9 +26,9 @@ export default function FriendListItem({ avatar, name, isOnline }) {
       <img src={avatar} alt="Avatar" width="48" className={styles.friendsImg} />
       <p className={styles.friendName}>{name}</p>
       <p
-        className={
-          (styles.friendStatus, isOnline ? styles.isOnline : styles.isOffline)
-        }
+        className={`${styles.friendStatus} ${
+          isOnline ? styles.isOnline : styles.isOffline
+        }`}
       >
         {isOnline ? 'Online' : 'Offline'}
       </p>
